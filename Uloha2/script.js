e6dcad1ba955016ad30cd9c0e5a3c5024dabb61c
X <X@x.x>
2025-11-20 10:45:11 +0100

    Ukoly 2 - 4

---
+++ Uloha2/script.js
@@ -0,0 +1,40 @@
+/* Uloha 1:
+let num1 = Number(prompt("Zadej cislo 1"))
+let num2 = Number(prompt("Zdj Cislo 2"))
+
+console.log("Vysledek", num1 + num2)
+*/
+
+/* Uloha2:
+let num = Number(prompt("Zadej cislo"))
+
+if(num % 2 == 0) {
+    console.log("Sudý")
+}
+else {
+    console.log("Lichy")
+}
+*/
+
+/* Uloha 3 
+
+let temp = Number(prompt("Zadej teplotu v °C"))
+
+console.log("Teplota v °F je: ", temp * 9/5 + 32)
+
+*/
+
+/* Uloha 4 
+
+let num3 = Number(prompt("Zadej cislo"))
+
+if (num3 == 0) {
+    console.log("Cislo je nula")
+}
+else if (num3 < 0) {
+    console.log("Cislo je zaporne")
+}
+else {
+    console.log("Cislo je kladne")
+}
+*/
